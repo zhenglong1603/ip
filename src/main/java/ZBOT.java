814f655f17f5b29edad706cc8bd3509ee9dc2117
zhenglong1603 <139528731+zhenglong1603@users.noreply.github.com>
2025-01-20 16:21:56 +0800

    added support for echo functionality

--- src/main/java/ZBOT.java
+++ src/main/java/ZBOT.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
 public class ZBOT {
-    public static void main(String[] args) {
+
+    public static void intro() {
         System.out.println("---------------------------------------------------");
-        System.out.println("Hello! I'm ZBOT");
+        System.out.println("Hello! I'm ZBOT!!");
         System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------------------");
+    }
+    public static void end() {
         System.out.println("---------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("---------------------------------------------------");
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        intro();
+
+        do {
+            input = scanner.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println("-----------------------");
+                System.out.println(input);
+                System.out.println("-----------------------");
+            }
+        } while (!input.equals("bye"));
+
+        end();
+    }
 }
+
