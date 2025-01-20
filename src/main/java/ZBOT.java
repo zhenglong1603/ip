24744411c6ac5083b146bad336244b1f4fdc8e7d
zhenglong1603 <139528731+zhenglong1603@users.noreply.github.com>
2025-01-20 15:44:52 +0800

    Added greet and exit feature and renamed the bot

---
+++ src/main/java/ZBOT.java
@@ -0,0 +1,10 @@
+public class ZBOT {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------");
+        System.out.println("Hello! I'm ZBOT");
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------------------");
+    }
+}
